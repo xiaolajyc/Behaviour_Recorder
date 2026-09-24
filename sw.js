4a2cee7b24075666bcc3f094cd43a435a0970a9c
xiaolajyc <72300443+xiaolajyc@users.noreply.github.com>
2026-09-25 03:15:05 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='personal-behavior-tracker-v19-shell';
+const CACHE_NAME='personal-behavior-tracker-v20-shell';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
